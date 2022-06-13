72ad619290696b0b9fec32c7c44a3e9cea8b4dea
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:05:57 -0300

    Update mundo01ex27.py

--- mundo01ex27.py
+++ mundo01ex27.py
@@ -1,3 +1,4 @@
+#Exercício Python 27: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 nome= str(input('digite seu nome'))
 dividido= nome.split()
 nome1= dividido[0:1]
