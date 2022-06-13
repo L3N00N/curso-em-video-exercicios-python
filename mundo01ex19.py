a1e80f944b97e0ad61b6ba4d7d031b3821e451ce
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:02:01 -0300

    Update mundo01ex19.py

--- mundo01ex19.py
+++ mundo01ex19.py
@@ -1,3 +1,4 @@
+#Exercício Python 19: Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
 from random import shuffle
 alun1= str(input('digite o 1º nome'))
 alun2= str(input('digite o 2º nome'))
