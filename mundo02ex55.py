298523e16eba0056bc1c898c4d9dbe3d96f78424
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:17:54 -0300

    Update mundo02ex55.py

--- mundo02ex55.py
+++ mundo02ex55.py
@@ -1,3 +1,4 @@
+#Exercício Python 55: Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 lista=[]
 for c in range(1, 6):
     lista.append(input('digite seu peso'))
