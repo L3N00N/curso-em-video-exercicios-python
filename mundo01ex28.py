7cfda17f895fca3b286290c2c162f1b19ebd2533
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:06:23 -0300

    Update mundo01ex28.py

--- mundo01ex28.py
+++ mundo01ex28.py
@@ -1,3 +1,4 @@
+#Exercício Python 28: Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
 from random import randint
 lista= randint(0, 5)
 escolhido= int(input('digite um numero entre 0 e 5'))
