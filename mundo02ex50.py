819e515b27371af229051426b8b05a2e04b85d13
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:15:47 -0300

    Update mundo02ex50.py

--- mundo02ex50.py
+++ mundo02ex50.py
@@ -1,3 +1,4 @@
+#Exercício Python 50: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 soma=0
 for c in range(1, 7):
     num=int(input('digite o {} numero'.format(c)))
