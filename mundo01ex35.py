ede8746f594be367640ccf69f70773b6545c01f3
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:08:40 -0300

    Update mundo01ex35.py

--- mundo01ex35.py
+++ mundo01ex35.py
@@ -1,3 +1,4 @@
+#Exercício Python 35: Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
 from math import m
 reta1= float(input('digite o valor de uma reta em cm'))
 reta2= float(input('digite outro valor de reta em cm'))
