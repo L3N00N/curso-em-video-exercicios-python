af8417e795cad640c0eb34e30997479a2748ce99
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:17:27 -0300

    Update mundo02ex54.py

--- mundo02ex54.py
+++ mundo02ex54.py
@@ -1,3 +1,4 @@
+#Exercício Python 54: Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
 from datetime import date
 ano_atual= date.today().year
 tot_maior=0
