9f556d4e556e860d16259e291d545c326693e59f
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:16:08 -0300

    Update mundo02ex51.py

--- mundo02ex51.py
+++ mundo02ex51.py
@@ -1,3 +1,4 @@
+#Exercício Python 51: Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
 primterm=int(input('digite o 1° da pa'))
 razao=int(input('digite a razão da pa'))
 print(primterm)
