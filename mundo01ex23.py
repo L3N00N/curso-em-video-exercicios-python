f5f297e45676192e8ba417ad5019e114e020f4a9
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:04:23 -0300

    Update mundo01ex23.py

--- mundo01ex23.py
+++ mundo01ex23.py
@@ -1,3 +1,4 @@
+#Exercício Python 23: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
 num= int(input('digite um numero entre 0 e 9999'))
 unid= num//1 % 10
 print(unid)
