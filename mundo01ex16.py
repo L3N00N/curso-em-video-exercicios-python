1b3a2c50119bdbe79b2fa08c21a38f2ac9291ac6
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:00:24 -0300

    Update mundo01ex16.py

--- mundo01ex16.py
+++ mundo01ex16.py
@@ -1,3 +1,4 @@
+#Exercício Python 16: Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
 num= float(input('digite um numero real'))
 num_int= int(num)
 #trunc
