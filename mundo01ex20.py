32a74efa5957c5869b5474144f0db789a6f017e8
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:02:34 -0300

    Update mundo01ex20.py

--- mundo01ex20.py
+++ mundo01ex20.py
@@ -1,3 +1,4 @@
+#Exercício Python 20: O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 from random import shuffle
 alun1= str(input('digite o 1º nome'))
 alun2= str(input('digite o 2º nome'))
