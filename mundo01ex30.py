9d39adbafcc2c1811acc7f0858af28a1b6ed9219
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:06:54 -0300

    Update mundo01ex30.py

--- mundo01ex30.py
+++ mundo01ex30.py
@@ -1,3 +1,4 @@
+#Exercício Python 30: Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
 num= int(input('digite um numero'))
 if (num%2)==1:
     print('o numero {} é impar'.format(num))
