9e96414b725dd4204f2d390a671ee28e603dfb29
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 15:36:04 -0300

    Update mundo01ex10.py

--- mundo01ex10.py
+++ mundo01ex10.py
@@ -1,3 +1,4 @@
+#Exercício Python 10: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 din= float(input('Quanto voce tem na carteira'))
 in_dolar= din/3.27
 print('Voce tem {} R$ e pode comprar {} dolares'.format(din, in_dolar))
