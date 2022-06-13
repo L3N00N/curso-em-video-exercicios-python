07039bfc9efbcd6dd0a114435e6e99d7a0eddccf
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:13:17 -0300

    Update mundo02ex37.py

--- mundo02ex37.py
+++ mundo02ex37.py
@@ -1,3 +1,4 @@
+#Exercício Python 37: Escreva um programa em Python que leia um número inteiro qualquer e peça para o usuário escolher qual será a base de conversão: 1 para binário, 2 para octal e 3 para hexadecimal.
 num= int(input('digite um numero inteiro'))
 escolha= int(input('escolha: \n 1 para binario \n 2 para octal \n 3 para exadecimal'))
 if escolha==1:
