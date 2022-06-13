b9fa632dd3f9e18c6caa53eb403752fd30bb23ee
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 15:59:44 -0300

    Update mundo01ex12.py

--- mundo01ex12.py
+++ mundo01ex12.py
@@ -1,3 +1,4 @@
+#Exercício Python 12: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 prod= float(input('digite o valor do produto'))
 prod2= prod-(prod*5/100)
 print('o valor do produto é de {} R$ e com o desconto de 5% fica {}'.format(prod, prod2))
