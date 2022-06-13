d4793ba538a570e8723dbc6a0dbf9c5ea885e1d8
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:17:02 -0300

    Update mundo02ex53.py

--- mundo02ex53.py
+++ mundo02ex53.py
@@ -1,3 +1,5 @@
+#Exercício Python 53: Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços. Exemplos de palíndromos:
+#APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA.
 frase= str(input('digite uma frase').upper().replace(' ', ''))
 frase2= frase[::-1]
 if frase==frase2:
