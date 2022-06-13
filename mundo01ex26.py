7b356691c51407e59c6751aa9c443bf5a482a298
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:05:31 -0300

    Update mundo01ex26.py

--- mundo01ex26.py
+++ mundo01ex26.py
@@ -1,3 +1,4 @@
+#Exercício Python 26: Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
 frase= str(input('digite uma frase')).upper().strip()
 print(frase.upper().count('a'))
 print(frase.upper().find('a')+1)
