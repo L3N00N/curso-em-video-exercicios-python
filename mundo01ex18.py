89b167001c5bd14858a6eab9ca1a9a8914ade0f7
Leonardo <102104510+L3N00N@users.noreply.github.com>
2022-06-13 16:01:22 -0300

    Update mundo01ex18.py

--- mundo01ex18.py
+++ mundo01ex18.py
@@ -1,3 +1,4 @@
+#Exercício Python 18: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
 from math import radians, sin, cos, tan
 ang= float(input('digite um angulo'))
 rad= radians(ang)
